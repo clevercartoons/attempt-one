654a95d113789e2c37ce2b3dcf525bb46b3feff9
clevercartoons <92776705+clevercartoons@users.noreply.github.com>
2022-10-30 15:46:24 +0200

    first commit
    initial commit with login and logout already working, next we push version controls once updated

---
+++ resources/views/admin/admin_master.blade.php
@@ -0,0 +1,99 @@
+
+<!DOCTYPE html>
+<html class="loading semi-dark-layout" lang="en" data-layout="semi-dark-layout" data-textdirection="ltr">
+    <!-- BEGIN: Head-->
+    <head>
+
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
+        <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+        <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
+        <meta name="author" content="PIXINVENT">
+        <title>Dashboard analytics - Vuexy - Bootstrap HTML admin template</title>
+        <link rel="apple-touch-icon" href="../../../app-assets/images/ico/apple-icon-120.png">
+        <link rel="shortcut icon" type="image/x-icon" href="../../../app-assets/images/ico/favicon.ico">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
+
+        <!-- BEGIN: Vendor CSS-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('backend/app-assets/vendors/css/vendors.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('backend/app-assets/vendors/css/charts/apexcharts.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('backend/app-assets/vendors/css/extensions/toastr.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('backend/app-assets/vendors/css/tables/datatable/dataTables.bootstrap4.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('backend/app-assets/vendors/css/tables/datatable/responsive.bootstrap.min.css') }}">
+        <!-- END: Vendor CSS-->
+
+        <!-- BEGIN: Theme CSS-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('backend/app-assets/css/bootstrap.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('backend/app-assets/css/bootstrap-extended.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('backend/app-assets/css/colors.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('backend/app-assets/css/components.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('backend/app-assets/css/themes/dark-layout.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('backend/app-assets/css/themes/bordered-layout.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('backend/app-assets/css/themes/semi-dark-layout.css') }}">
+
+        <!-- BEGIN: Page CSS-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('backend/app-assets/css/core/menu/menu-types/vertical-menu.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('backend/app-assets/css/plugins/charts/chart-apex.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('backend/app-assets/css/plugins/extensions/ext-component-toastr.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('backend/app-assets/css/pages/app-invoice-list.css') }}">
+        <!-- END: Page CSS-->
+
+        <!-- BEGIN: Custom CSS-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('backend/assets/css/style.css') }}">
+        <!-- END: Custom CSS-->
+
+    </head>
+    <!-- END: Head-->
+
+    <!-- BEGIN: Body-->
+
+<body class="vertical-layout vertical-menu-modern  navbar-floating footer-static  " data-open="click" data-menu="vertical-menu-modern" data-col="">
+            <!-- BEGIN: Header-->
+            @include('admin.body.header')
+            <!-- END: Header-->
+            <!-- BEGIN: Main Menu-->
+            @include('admin.body.sidebar')
+            <!-- END: Main Menu-->
+            <!-- BEGIN: Content-->
+                @yield('content')
+            <!-- END: Content-->
+            <div class="sidenav-overlay"></div>
+            <div class="drag-target"></div>
+            <!-- BEGIN: Footer-->
+            @include('admin.body.footer')
+            <!-- END: Footer-->
+    <!-- BEGIN: Vendor JS-->
+    <script src="{{ asset('backend/app-assets/vendors/js/vendors.min.js') }}"></script>
+    <!-- BEGIN Vendor JS-->
+    <!-- BEGIN: Page Vendor JS-->
+    <script src="{{ asset('backend/app-assets/vendors/js/charts/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('backend/app-assets/vendors/js/extensions/toastr.min.js') }}"></script>
+    <script src="{{ asset('backend/app-assets/vendors/js/extensions/moment.min.js') }}"></script>
+    <script src="{{ asset('backend/app-assets/vendors/js/tables/datatable/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('backend/app-assets/vendors/js/tables/datatable/datatables.buttons.min.js') }}"></script>
+    <script src="{{ asset('backend/app-assets/vendors/js/tables/datatable/datatables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('backend/app-assets/vendors/js/tables/datatable/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('backend/app-assets/vendors/js/tables/datatable/responsive.bootstrap4.min.js') }}"></script>
+    <!-- END: Page Vendor JS-->
+    <!-- BEGIN: Theme JS-->
+    <script src="{{ asset('backend/app-assets/js/core/app-menu.js') }}"></script>
+    <script src="{{ asset('backend/app-assets/js/core/app.js') }}"></script>
+    <!-- END: Theme JS-->
+    <!-- BEGIN: Page JS-->
+    <script src="{{ asset('backend/app-assets/js/scripts/pages/dashboard-analytics.js') }}"></script>
+    <script src="{{ asset('backend/app-assets/js/scripts/pages/app-invoice-list.js') }}"></script>
+    <!-- END: Page JS-->
+    <script>
+        $(window).on('load', function() {
+            if (feather) {
+                feather.replace({
+                    width: 14,
+                    height: 14
+                });
+            }
+        })
+    </script>
+</body>
+<!-- END: Body-->
+</html>
---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,11 @@
+
+
+    <footer class="footer footer-static footer-light">
+        <p class="clearfix mb-0"><span class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2022
+
+            <a class="ml-25" href="https://shark-app-7h9gy.ondigitalocean.app/" target="_blank">L.A.T.I.T.U.D.E</a>
+                <span class="d-none d-sm-inline-block">, All rights Reserved</span></span>
+                <span class="float-md-right d-none d-md-block">Made With<i data-feather="heart"></i> And Hand-Crafted</span>
+        </p>
+    </footer>
+    <button class="btn btn-primary btn-icon scroll-top" type="button"><i data-feather="arrow-up"></i></button>
